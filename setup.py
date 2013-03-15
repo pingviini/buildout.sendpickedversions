1a74cef13c701dc9fdced1abe477f76861f4e09c
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2013-03-15 18:26:23 +0200

    CHANGES.txt -> CHANGES.rst. pep8 check for setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 version = "1.0a1"
 
@@ -16,31 +16,31 @@ requires = ['setuptools', 'zc.buildout']
 if sys.version[:3] < '2.6':
     requires.append('simplejson')
 
-tests_require=['zc.buildout', 'zope.testing', 'zc.recipe.egg']
+tests_require = ['zc.buildout', 'zope.testing', 'zc.recipe.egg']
 
 setup(name='buildout.sendpickedversions',
       version=version,
       description="Sends picked packages and versions to a whiskers server.",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        'Framework :: Buildout',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
+          'Framework :: Buildout',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       license='GPL',
       keywords='buildout extension send picked versions',
       author='Jukka Ojaniemi',
       author_email='jukka.ojaniemi@jyu.fi',
       url='http://github.com/collective/buildout.sendpickedversions',
-      packages = find_packages('src'),
-      package_dir = {'':'src'},
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       namespace_packages=['buildout'],
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      test_suite = 'buildout.sendpickedversions.tests.test_suite',
+      test_suite='buildout.sendpickedversions.tests.test_suite',
       entry_points=entry_points,
       )
